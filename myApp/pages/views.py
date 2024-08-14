421963f86bd2ec55327e88ba57964c150c89de11
Mahmoud-Duhaidi <mahmoudduhaidi1@gmail.com>
2024-08-14 23:54:28 +0300

    update view

--- myApp/pages/views.py
+++ myApp/pages/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
-
 from django.shortcuts import HttpResponse
 from django.contrib import messages
 from . import models
@@ -19,19 +18,17 @@ def success(request):
 def index(request):
     return render(request,"login.html")
 
-def create_user(fname, lname, email, passwd):
+def create_user(fname, lname, email, phone, passwd):
     salt = bcrypt.gensalt()
     enc_pass = bcrypt.hashpw(passwd.encode(), salt).decode()
     new_user = models.Users.objects.create(
         first_name=fname,
         last_name=lname,
         email=email,
+        phone=phone,
         password=enc_pass
     )
     return new_user
-
-
-
 
 def register(request):
     if request.method == "POST":
@@ -47,16 +44,16 @@ def register(request):
             email1 = request.POST['email']
             password = request.POST['password']
             password_confirmation=request.Post['password_confirmation']
+            if password != password_confirmation:
+                messages.error(request, "Passwords do not match.")
+                return redirect('/zo/register')
+
             
             user = create_user(first_name1, last_name1,phone, email1, password,password_confirmation)
             print('Created3')
             request.session['fname'] = user.first_name
             request.session['lname'] = user.last_name
-            request.session['phone'] = user.phone
             request.session['email'] = user.last_namerequest.session['first_name'] = user.first_name
-            request.session['password'] = user.password
-            request.session['password_confirmation'] = user.password_confirmation
-
             print('Created4')
 
     return render(request, 'register.html')
@@ -67,8 +64,11 @@ def login(request):
         password = request.POST['mypassword']
         user = models.Users.objects.filter(email=email).first()
         if user and bcrypt.checkpw(password.encode(), user.password.encode()):
-            request.session['fname'] = user.first_name
-            request.session['lname'] = user.last_name
+            request.session['user'] = {
+                'fname': user.first_name,
+                'lname': user.last_name,
+                'email': user.email
+            }
             return redirect('/zo/success')
         else:
             messages.error(request, "Invalid email or password.")
